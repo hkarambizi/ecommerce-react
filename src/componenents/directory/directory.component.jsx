31b17b75715f06402692c85e3402fe2fc4ca3147
hkarambizi <hkarambizi@gmail.com>
2019-07-30 10:19:27 -0400

    update menu-items to navigate using withRouter

--- src/componenents/directory/directory.component.jsx
+++ src/componenents/directory/directory.component.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import { sections } from "./directory.data";
-import { MenuItem } from "./../menu-item/menu-item.component";
+import MenuItem from "./../menu-item/menu-item.component";
 
 export default class Directory extends Component {
   constructor(props) {
@@ -13,8 +13,8 @@ export default class Directory extends Component {
   render() {
     return (
       <div className="directory-menu">
-        {this.state.sections.map(({ id, imageUrl, title, size }) => (
-          <MenuItem key={id} title={title} image={imageUrl} size={size} />
+        {this.state.sections.map(({ id, ...otherSectionProps }) => (
+          <MenuItem key={id} {...otherSectionProps} />
         ))}
       </div>
     );
--- src/componenents/menu-item/menu-item.component.jsx
+++ src/componenents/menu-item/menu-item.component.jsx
@@ -1,11 +1,14 @@
 import React from "react";
+import { withRouter } from "react-router-dom";
 import "./menu-item.styles.scss";
 
-export const MenuItem = ({ title, image, size }) => (
-  <div className={`${size} menu-item`}>
+const MenuItem = ({ title, imageUrl, size, history, linkUrl, match }) => (
+  <div
+    className={`${size} menu-item`}
+    onClick={() => history.push(`${match.url}${linkUrl}`)}>
     <div
       className="background-image"
-      style={{ backgroundImage: `url(${image})` }}
+      style={{ backgroundImage: `url(${imageUrl})` }}
     />
     <div className="content">
       <h1 className="title">{title.toUpperCase()}</h1>
@@ -13,3 +16,5 @@ export const MenuItem = ({ title, image, size }) => (
     </div>
   </div>
 );
+
+export default withRouter(MenuItem);
