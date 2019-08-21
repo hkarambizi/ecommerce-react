148569498883917d523dd8871304c3721dd91eea
hkarambizi <hkarambizi@gmail.com>
2019-08-20 21:03:39 -0400

    added server and stripe backend to make payment chargerequests

--- client/src/components/stripe-button/stripe-button.component.jsx
+++ client/src/components/stripe-button/stripe-button.component.jsx
@@ -1,12 +1,28 @@
 import React from "react";
 import StripeCheckout from "react-stripe-checkout";
+import axios from "axios";
 const StripeCheckoutButton = ({ price }) => {
   const priceConvertedForStripe = price * 100; // in cents
   const publishableKey = "pk_test_6PADT7ui7H2G7957bJ5mW48R00mfJDMSnC";
 
   const onToken = token => {
-    console.log(token);
-    alert("Payment Successfull");
+    axios({
+      method: "POST",
+      url: "payment",
+      data: {
+        amount: priceConvertedForStripe,
+        token
+      }
+    })
+      .then(response => {
+        alert("Payment was successful!");
+      })
+      .catch(error => {
+        console.log("Payment error: ", JSON.parse(error));
+        alert(
+          "Payment error occured. Please use the provided test credit card data"
+        );
+      });
   };
   return (
     <StripeCheckout
@@ -14,7 +30,7 @@ const StripeCheckoutButton = ({ price }) => {
       name="Pop Shop"
       billingAddress
       shippingAddress
-      image="https://svgshare.com/i/CUz.svg"
+      image="https://image.flaticon.com/icons/png/128/607/607702.png"
       description={`Your total is $${price}`}
       amount={priceConvertedForStripe}
       panelLabel="Pay Now"
